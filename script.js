1a2034e6130e845d2af51014e152070f1831180c
Dimitris Livanas <livanas.dimitris@gmail.com>
2023-04-01 17:20:23 +0200

    create and test function 'getPlayerChoice'

--- script.js
+++ script.js
@@ -6,9 +6,37 @@ let computerSelection;
 
 /* Function that randomly returns "Rock", "Paper" or "Scissors" */
 function getComputerChoice() {
-    const choiceList = [r,p,s];
-    let computerSelection = choiceList[Math.floor(Math.random()*choiceList.length)];
-    console.log(computerSelection);
+  const choiceList = [r, p, s];
+  let computerSelection =
+    choiceList[Math.floor(Math.random() * choiceList.length)];
+  console.log(`The computer chose ${computerSelection}`);
 }
 
-getComputerChoice();
+/* Function that asks the player to choose r for Rock, p for Paper or S for Scissors */
+function getPlayerChoice() {
+  let playerSelection = prompt(
+    "Please choose R for 'Rock', P for 'Paper' or S for 'Scissors'"
+  ).toLowerCase();
+
+  switch (playerSelection) {
+    case "r":
+      playerSelection = r;
+      console.log(`You chose ${playerSelection}`);
+      break;
+
+    case "p":
+      playerSelection = p;
+      console.log(`You chose ${playerSelection}`);
+      break;
+
+    case "s":
+      playerSelection = s;
+      console.log(`You chose ${playerSelection}`);
+      break;
+
+    default:
+      console.log("You need to choose one of the options, R, P or S");
+  }
+}
+
+getPlayerChoice();
