264391d2633ba52c9292a296f815e7040a1006b0
Dimitris Livanas <livanas.dimitris@gmail.com>
2023-04-01 16:47:42 +0200

    create global variables and getComputerChoice function

---
+++ script.js
@@ -0,0 +1,10 @@
+let r = "Rock";
+let p = "Paper";
+let s = "Scissors";
+let playerSelection;
+let computerSelection;
+
+/* Function that randomly returns "Rock", "Paper" or "Scissors" */
+function getComputerChoice() {
+    
+}
